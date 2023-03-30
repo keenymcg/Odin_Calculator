d7e8df1da5b01550b7aace2426a4aaa04525ebb3
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-03-30 11:30:20 -0700

    Add calc design

--- main.js
+++ main.js
@@ -1,36 +1,11 @@
 /*  JS TODO
 
-create functions for all the operators
-
-each time a num-op-num combination is clicked, it stores that answer in a variable, which updates on the display
-thereafter (in same function?), each time an operator is pressed, it uses the previously calculated variable
-
-maybe push all click-events to an array, 
-For loop (where # of iterations is based on length of array) 
-
-When = is clicked, join all elements of the array, run the calculation 
-
-
-specific functions for if its clicked; a number or an operator
-
 ~~ number function
 event listener: a click concatenates a num (as a string) to a variable
 - variable can only be 9 digits long
 --- therefore it checks for the num1 string-length. If it === 9, don't concatenate
 
-when an operator is pressed, the operator is stored in an array? thinking we use a template literal to pull from the array?
-
-a new num variable is created (num2) which will be used after
-
-
-if a number is clicked, check to see if the var that holds operator is blank. If so, num1 variable is activated.
-Clicks and subsequent number-clicks are concatenated to the num1 variable
-Send that variable to the screen? (or store in a "display" variable that constantly gets updated and pushed to the screen)
-
-on subsequent number click-events, it checks if there's an operator stored, and if so...
-...num2 is created, and number-clicks are stored/concat-ed to that num2 var
-
-OPERATOR FUNCTION (using * as example)
+OPERATOR FUNCTION (using * as example) - DONE?
 clicks on the operator should act as an equal sign at first (for the first two numbers), so that num1 is the accumulator
 in the first click-event, it'll return a num1 times an empty num2
 - if num1 * num2 != number, don't do anything
@@ -66,6 +41,8 @@ function operatorClick(operator) { //change this to a click-activated function
         result = parseFloat(num1) * parseFloat(num2);
     } else if (operator === '/') {
         result = parseFloat(num1) / parseFloat(num2);
+    } else if (operator === '^') {
+        result = parseFloat(num1) ** parseFloat(num2);
     };
 
     num1 = result
@@ -75,10 +52,10 @@ function operatorClick(operator) { //change this to a click-activated function
 };
 
 function numClick(value) { // might need to store this function inside the num1 and num2 parsefloat parameters
-    if (currentOperator == "") {
+    if (currentOperator == "" && num1.length < 9) {
         num1 = num1.concat(value);
         return num1;
-    } else if (currentOperator != "") {
+    } else if (currentOperator != "" && num2.length < 9) {
         num2 = num2.concat(value);
         return num2;
     };
