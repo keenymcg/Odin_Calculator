0f7cc463e75ffc0ff7651d9d687aa11985a52150
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-04-01 11:35:48 -0700

    Fix display() so num2 displays

--- main.js
+++ main.js
@@ -1,27 +1,6 @@
-/*  JS TODO
-
-OPERATOR FUNCTION (using * as example) - DONE? NO
-clicks on the operator should act as an equal sign at first (for the first two numbers), so that num1 is the accumulator
-in the first click-event, it'll return a num1 times an empty num2
-- if num1 * num2 != number, don't do anything
-Then num2 is clicked, and still nothing visibly happens except: 
-- IN SEPARATE FUNC? checks to see if there's a value in the operator variable, and...
-- ...if there is, num2 is wiped to empty string
-- then the new number is concatenated to the newly-empty num2 variable (don't forget max of 9 digits/chars)
-then assume the operator is clicked again
-it performs the equation, now with num2 having a number
-sends that number to the screen
-stores the result in num1
-on subsequent number click-events, it checks if there's an operator stored, and if so...
-...num2 is populated with the new number, and number-clicks are stored/concat-ed to that num2 var
-And then that just repeats
-*/
-
-
 
 let num1 = ''
 let num2 = ''
-let temp = 0
 let currentOperator = ''
 
 const container = document.querySelector('#calcContainer');
@@ -40,16 +19,6 @@ allButtons.forEach(div => {
         
     });
 });
-
-/* function numClick(value) { // might need to store this function inside the num1 and num2 parsefloat parameters
-    if (currentOperator == '' && num1.length < 9) {
-        num1 = num1.concat(value);
-        return num1;
-    } else if (currentOperator != "" && num2.length < 9) {
-        num2 = num2.concat(value);
-        return num2;
-    };
-}; */
 
 function buttonClick(div) {
     let textContent = div.textContent;
@@ -77,6 +46,7 @@ function buttonClick(div) {
             clearAll();
         };
     };
+    display();
     console.log(`this is num1: ${num1}`)
     console.log(`this is num2: ${num2}`);
 };
@@ -84,7 +54,7 @@ function buttonClick(div) {
 function display(num2) {
     let display = document.getElementById('display')
     display.textContent = num1
-    if (num2 = '') {
+    if (num2 === '') {
         display.textContent = num1; 
     } else if (num2 != '') {
         display.textContent = num2;
@@ -95,15 +65,17 @@ let numsFilled = false
 function checkNum1andNum2() {
     if (num1 != '' && num2 != '') {
         numsFilled = true;
+    } else {
+        numsFilled = false;
     };
 };
 
-function operatorClick(operator) {
-    checkNum1andNum2();
+function operatorClick(operator) { //TODO: Can't figure out why the conditionals are being bypassed. Check your ChatGPT convo
+    checkNum1andNum2;
     let result = num1;
     if (numsFilled = false) {
         result = num1;
-    } else if (numsFilled === true && operator === '+') {
+    } else if (numsFilled && operator === '+') {
         result = parseFloat(num1) + parseFloat(num2);
     } else if (numsFilled === true && operator === '-') {
         result = parseFloat(num1) - parseFloat(num2);
@@ -119,14 +91,9 @@ function operatorClick(operator) {
     console.log(currentOperator);
     num1 = result
     num2 = '' 
+    console.log(`this is the result: ${result}`);
 }
-    // maybe put all of this inside an "if num2 = '' don't do anything, BUT..."
-    // ...if num2 != 0, do something
-    // remember trying to put all this into a template literal, the create function to run equation?
 
-
-// need a clearAll() to reset the calculator
-// ideally clicking it once just resets the current variable, and twice resets everything
 // if clearAll() is run when the display has text-content, clear the display? or go back to the prior result (num1)...
     // ...with a little message that says "previous input..."
 // if num2 is an empty string, then num1 = '', and clear the operator variable? clear num2 var for good measure. 
