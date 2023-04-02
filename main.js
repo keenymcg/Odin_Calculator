7fa3533c90d2ed3e521db7936594ab30feb138f4
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2023-04-01 19:08:39 -0700

    Debug equals prob, put in reg expression

--- main.js
+++ main.js
@@ -16,21 +16,26 @@ const allButtons = document.querySelectorAll('.button');
 allButtons.forEach(div => {
     div.addEventListener('click', () => {
         buttonClick(div);
-        // console.log(div.textContent);
         display(num2);
         
     });
 });
 
-function buttonClick(div) { //TODO: Why does operator function double-click each first time an operator is clicked
+// WHY does the equals sign just work? -- just figured out, it was in the num2 operators reg expression
+// when clicked, it updates num1 and empties num2
+// then you click a new number, it populates num2, and you hit equals
+// it then empties num2 and returns to num1, BUT... I want hitting = in this situation to now change num1 to num2
+
+function buttonClick(div) {
     let textContent = div.textContent;
+//    equals(textContent);
     if (prevOperator === '') {
         if (/[0-9]/.test(textContent) && num1.length <= 8) {
             // console.log('The number is between 1-9');
             num1 = num1.concat(textContent);
         } else if (/[.]/.test(textContent) && num1.indexOf('.') == -1) {
             num1 = num1.concat(textContent);
-        } else if (/[/\+\-X]/.test(textContent)) {
+        } else if (/[/\+\-X^]/.test(textContent)) {
             operatorClick(textContent);
         } else if (textContent === '~') { 
             num1 = num1 * -1;
@@ -44,7 +49,7 @@ function buttonClick(div) { //TODO: Why does operator function double-click each
             num2 = num2.concat(textContent);
         } else if (/[.]/.test(textContent) && num2.indexOf('.') == -1) {
             num2 = num2.concat(textContent);
-        } else if (/[/\+\-X=]/.test(textContent)) {
+        } else if (/[/\+\-X^=]/.test(textContent)) {
             operatorClick(textContent);
         } else if (textContent === '~') { 
             posNegNum2('~');
@@ -55,6 +60,12 @@ function buttonClick(div) { //TODO: Why does operator function double-click each
     console.log(`this is num1: ${num1}`)
     console.log(`this is num2: ${num2}`);
 };
+
+/* function equals(equal) {
+    if (equal === '=' && num2 != '') {
+        num1 = num2;
+    };
+}; */
 
 function posNegNum2(posNeg) {
     if (num2 === '' || num2 === 0) {
@@ -83,18 +94,8 @@ function checkNum1andNum2() {
     };
 };
 
-// FIXING OPERATOR FUNCTION
-// when the operator is clicked, currentOperator = the click
-// if the prevOperator is '' do nothing
-// ... if the preOperator is +-/X, run the equation according to that operator
-// 
-
 function operatorClick(operator) {
     let numsFilled = checkNum1andNum2();
-
-    // console.log(typeof operator);
-    console.log(numsFilled);
-    // console.log(parseFloat(num1));
 
     let result = num1;
     if (numsFilled === true && prevOperator === '+') {
@@ -110,36 +111,28 @@ function operatorClick(operator) {
     } else if (numsFilled === false) {
         result = num1;
     };
-    prevOperator = operator
+    
+    if (operator != '=') {
+        prevOperator = operator
+    };
+
     num1 = result
     num2 = '' 
-    console.log(`this is the result: ${result}`);
+    // console.log(`this is the result: ${result}`);
 };
 
 function clearAll() {
     console.log("You've run clearAll()")
-    // let display = document.getElementById('display')
+
     if (num1 != '' && num2 != '') {
         num2 = '';
-    } else if (num1 != '' && num2 === '') {
+    } else if ((num1 != '' && num2 === '') ||
+    (num1 === 0 && num2 === '') || 
+    (num1 === '' && num2 != '') ||
+    (num1 === 0 && num2 != '')) {
         num1 = '';
         num2 = '';
         currentOperator = '';
         prevOperator = '';
-    } else if (num1 === 0 && num2 === '') {
-        num1 = '';
-        num2 = '';
-        currentOperator = '';
-        prevOperator = '';
-    } else if (num1 === '' && num2 != '') {
-        num1 = '';
-        num2 = '';
-        currentOperator = '';
-        prevOperator = '';
-    } else if (num1 === 0 && num2 != '') {
-        num1 = 0;
-        num2 = '';
-        currentOperator = '';
-        prevOperator = '';
-    }
+    };
 };
